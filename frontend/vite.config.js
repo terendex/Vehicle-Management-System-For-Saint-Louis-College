2b6c380bf4173ef12d10e663c681cfa598c3bf59
Twilightsveil <joshuavillareal.business@gmail.com>
2026-06-30 14:31:09 +0800

    allowed ngrok in vite config to host temporarily

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['preconcurrently-inorganic-nicolle.ngrok-free.dev'],
     proxy: {
       // MJPEG streaming endpoint — no timeout, no buffering
       '/api/vehicles/parking-zones': {
